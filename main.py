46f37734050f481d7c91896c9a1b281a6de8ad0b
Benja Share <benja1006@gmail.com>
2022-05-02 01:11:13 -0700

    clicking now draws a circle on the slide. Press s to go forwards and a to go backwards and q to quit

--- main.py
+++ main.py
@@ -18,16 +18,11 @@ import screeninfo
 #     if (cv2.waitKey(1) & 0xFF) == ord('q'):
 #         cv2.destroyAllWindows()
 #         break
-# def onMouse(event, x, y, flags, param):
-#     if event == cv2.EVENT_LBUTTONDOWN:
-#         if (cv2.waitKey(1) & 0xFF) == ord('q'):
-#             print("'top': " + y)
-#         if (cv2.waitKey(1) & 0xFF) == ord('e'):
-#             print("bottom: " + y)
-#         if (cv2.waitKey(1) & 0xFF) == ord('w'):
-#             print("right: " + x)
-#         if (cv2.waitKey(1) & 0xFF) == ord('r'):
-#             print("'left': " + x)
+def onMouse(event, x, y, flags, param):
+    global mouse_x, mouse_y, draw
+    if event == cv2.EVENT_LBUTTONDOWN:
+        circle = cv2.circle(images[i], (x, y), 20, (0,0,255), 2)
+        cv2.imshow("test", circle)
 # while True:
 #     if (cv2.waitKey(1) & 0xFF) == ord('l'):
 #             cv2.destroyAllWindows()
@@ -35,21 +30,25 @@ import screeninfo
 #
 #
 #
-#     cv2.setMouseCallback('WindowName', onMouse)
 screen = screeninfo.get_monitors()[0]
+cv2.namedWindow("test", cv2.WINDOW_NORMAL)
+cv2.moveWindow("test", screen.x - 1, screen.y - 1)
+cv2.setWindowProperty("test", 0, 1)
+cv2.setMouseCallback('test', onMouse)
+
 imageDirectory = os.path.join(os.getcwd(), "images")
 images = [cv2.imread(os.path.join(imageDirectory, f)) for f in os.listdir(imageDirectory) if os.path.isfile(os.path.join(imageDirectory, f))];
 i = 0;
 while True:
-    cv2.namedWindow("test", cv2.WINDOW_NORMAL)
-    cv2.moveWindow("test", screen.x - 1, screen.y - 1)
-    cv2.setWindowProperty("test", 0, 1)
+
     cv2.imshow("test", images[i])
+
+    cv2.setMouseCallback('test', onMouse)
     k = cv2.waitKey(0)
 
-    if k == ord("s"):
+    if k == ord("s") and i < (len(images)-1):
         i += 1
-        if i == len(images):
-            break
+    if k == ord("a") and i > 0:
+        i-= 1
     if k == ord("q"):
         break
