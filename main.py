8fccabccf48f401ebc0b049e408c0f8b6e591bbc
Benja Share <benja1006@gmail.com>
2022-05-03 02:30:59 -0700

    now draws lines when clicking and holding

--- main.py
+++ main.py
@@ -19,10 +19,20 @@ import screeninfo
 #         cv2.destroyAllWindows()
 #         break
 def onMouse(event, x, y, flags, param):
-    global mouse_x, mouse_y, draw
+    global start_x, start_y, draw
     if event == cv2.EVENT_LBUTTONDOWN:
-        circle = cv2.circle(images[i], (x, y), 20, (0,0,255), 2)
-        cv2.imshow("test", circle)
+        #circle = cv2.circle(images[i], (x, y), 20, (0,0,255), 2)
+        #cv2.imshow("test", circle)
+        if draw == False:
+            start_x, start_y = x, y
+            draw = True
+            return
+    elif event == cv2.EVENT_LBUTTONUP:
+        draw = False
+        line = cv2.line(images[i],(start_x, start_y), (x,y), (0,0,255), 2)
+        cv2.imshow("test", line)
+
+
 # while True:
 #     if (cv2.waitKey(1) & 0xFF) == ord('l'):
 #             cv2.destroyAllWindows()
@@ -35,7 +45,7 @@ cv2.namedWindow("test", cv2.WINDOW_NORMAL)
 cv2.moveWindow("test", screen.x - 1, screen.y - 1)
 cv2.setWindowProperty("test", 0, 1)
 cv2.setMouseCallback('test', onMouse)
-
+draw = False;
 imageDirectory = os.path.join(os.getcwd(), "images")
 images = [cv2.imread(os.path.join(imageDirectory, f)) for f in os.listdir(imageDirectory) if os.path.isfile(os.path.join(imageDirectory, f))];
 i = 0;
