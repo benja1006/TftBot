b3c8ad6d5235f8a33fe43526e3075e6926fd9cc3
benja <benja1006@gmail.com>
2022-09-21 23:24:02 -0700

    moved coord recording to new file. Cleared out main

---
+++ getCoords.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
+import sys
+import os
+import screeninfo
+
+def onMouse(event, x, y, flags, param):
+    global start_x, start_y, draw
+    if event == cv2.EVENT_LBUTTONDOWN:
+
+        if not draw:
+            start_x, start_y = x, y
+            print(str(x) + " " + str(y))
+            draw = True
+            return
+    elif event == cv2.EVENT_LBUTTONUP:
+        draw = False
+        line = cv2.rectangle(images[i], (start_x, start_y), (x, y), (0, 0, 255), 2)
+        cv2.imshow("test", line)
+        print(str(x) + " " + str(y))
+
+
+screen = screeninfo.get_monitors()[0]
+cv2.namedWindow("test", cv2.WINDOW_NORMAL)
+cv2.moveWindow("test", screen.x - 1, screen.y - 1)
+cv2.setWindowProperty("test", 0, 1)
+cv2.setMouseCallback('test', onMouse)
+draw = False
+imageDirectory = os.path.join(os.getcwd(), "images")
+images = [cv2.imread(os.path.join(imageDirectory, f)) for f in os.listdir(imageDirectory) if os.path.isfile(os.path.join(imageDirectory, f))];
+i = 0
+while True:
+
+    cv2.imshow("test", images[i])
+
+    cv2.setMouseCallback('test', onMouse)
+    k = cv2.waitKey(0)
+
+    if k == ord("s") and i < (len(images)-1):
+        i += 1
+    if k == ord("a") and i > 0:
+        i -= 1
+    if k == ord("q"):
+        break
--- main.py
+++ main.py
@@ -1,53 +1,26 @@
-import numpy as np
 import cv2
-from mss import mss
-from PIL import Image
-import sys
 import os
 import screeninfo
 
 
-# bounding_box = {'top': 100, 'left': 0, 'width': 400, 'height': 300}
-#
-# sct = mss()
-
-# while True:
-#     sct_img = sct.grab(bounding_box)
-#     cv2.imshow('screen', np.array(sct_img))
-#
-#     if (cv2.waitKey(1) & 0xFF) == ord('q'):
-#         cv2.destroyAllWindows()
-#         break
 def onMouse(event, x, y, flags, param):
-    global start_x, start_y, draw
-    if event == cv2.EVENT_LBUTTONDOWN:
-
-        if not draw:
-            start_x, start_y = x, y
-            print(str(x) + " " + str(y))
-            draw = True
-            return
-    elif event == cv2.EVENT_LBUTTONUP:
-        draw = False
-        line = cv2.rectangle(images[i], (start_x, start_y), (x, y), (0, 0, 255), 2)
-        cv2.imshow("test", line)
-        print(str(x) + " " + str(y))
+    return
 
 
 screen = screeninfo.get_monitors()[0]
-cv2.namedWindow("test", cv2.WINDOW_NORMAL)
-cv2.moveWindow("test", screen.x - 1, screen.y - 1)
-cv2.setWindowProperty("test", 0, 1)
-cv2.setMouseCallback('test', onMouse)
+cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+cv2.moveWindow("image", screen.x - 1, screen.y - 1)
+cv2.setWindowProperty("image", 0, 1)
+cv2.setMouseCallback('image', onMouse)
 draw = False
 imageDirectory = os.path.join(os.getcwd(), "images")
 images = [cv2.imread(os.path.join(imageDirectory, f)) for f in os.listdir(imageDirectory) if os.path.isfile(os.path.join(imageDirectory, f))];
 i = 0
 while True:
 
-    cv2.imshow("test", images[i])
+    cv2.imshow("image", images[i])
 
-    cv2.setMouseCallback('test', onMouse)
+    cv2.setMouseCallback('image', onMouse)
     k = cv2.waitKey(0)
 
     if k == ord("s") and i < (len(images)-1):
