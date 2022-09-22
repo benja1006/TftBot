2e3bfb2dfd38f1bc79cb1e644744312856cf7e2d
benja <benja1006@gmail.com>
2022-09-22 00:41:26 -0700

    trying to make input read across whole os. pyinput doesn't work on mac

--- main.py
+++ main.py
@@ -1,42 +1,65 @@
 import cv2
 import os
 import screeninfo
+import pyautogui
+import numpy as np
+import time
+from pyinput import keyboard
 
-
-def onMouse(event, x, y, flags, param):
-    return
-
-
-screen = screeninfo.get_monitors()[0]
-cv2.namedWindow("image", cv2.WINDOW_NORMAL)
-cv2.moveWindow("image", screen.x - 1, screen.y - 1)
-cv2.setWindowProperty("image", 0, 1)
-cv2.setMouseCallback('image', onMouse)
-draw = False
-imageDirectory = os.path.join(os.getcwd(), "images")
-images = [cv2.imread(os.path.join(imageDirectory, f)) for f in os.listdir(imageDirectory) if os.path.isfile(os.path.join(imageDirectory, f))];
 
 yTop = 1237
 yBottom = 1425
 xStart = 640
 xWidth = 257
 xSpacing = 268
-for i, image in enumerate(images):
-    for x in range(0, 5):
-        rect = cv2.rectangle(image, (xStart + x * xSpacing, yTop), (xStart + xWidth + x * xSpacing, yBottom), (0, 0, 255), 2)
-        cv2.imshow('image', rect)
+shopPath = os.path.join(os.getcwd(), "shopChamps")
 
-i = 0
+
+# keyboard listening
+def on_press(key):
+    if key == keyboard.Key.d:
+        time.sleep(.1)
+        screenGrabShop()
+
+
+def on_release(key):
+    if key == keyboard.Key.q:
+        return False
+
+
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+
+def screenGrabShop():
+    indexes = [os.path.join(shopPath, f)[0:-4] for f in os.listdir(shopPath) if os.path.isFile(os.path.join(shopPath, f))]
+    currIndex = 0
+    if indexes != []:
+        currIndex = max(indexes)
+    image = pyautogui.screenshot()
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    for i in range(0, 5):
+        champ = image[yTop:yBottom, xStart + i * xSpacing:xStart + xWidth + i * xSpacing]
+        cv2.imwrite(str(os.path.join(shopPath, str(currIndex))) + ".jpg", champ)
+        currIndex += 1
+
+
+cv2.namedWindow("test", cv2.WINDOW_NORMAL)
+
 while True:
 
-    cv2.imshow("image", images[i])
-
-    cv2.setMouseCallback('image', onMouse)
     k = cv2.waitKey(0)
 
-    if k == ord("s") and i < (len(images)-1):
-        i += 1
-    if k == ord("a") and i > 0:
-        i -= 1
+    if k == ord("d"):
+        time.sleep(.1)
+        screenGrabShop()
     if k == ord("q"):
         break
