6b95ee0068a71e47b1712d9a2d5e6cae3433b347
benja <benja1006@gmail.com>
2022-09-23 19:46:06 -0700

    imageRecognition

---
+++ imageRecognition.py
@@ -0,0 +1,70 @@
+import numpy as np
+from tensorflow import keras
+from keras.constraints import maxnorm
+from keras.utils import np_utils
+import os
+
+seed = 21
+
+# import data from folders
+champ_ds = keras.utils.image_dataset_from_directory(
+    directory='champs',
+    labels='inferred',
+    image_size=(257, 188),
+    seed=seed,
+    validation_split=.2,
+    subset='both'
+)
+
+(X_train, y_train), (X_test, y_test) = champ_ds
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+class_num = y_test.shape[1]
+
+model = keras.Sequential()
+model.add(keras.layers.Conv2D(32, (3, 3), input_shape=X_train.shape[1:],
+                              padding='same'))
+model.add(keras.layers.Activation('relu'))
+
+model.add(keras.layers.Conv2D(32, 3, input_shape=(32, 32, 3),
+                              activation='relu', padding='same'))
+model.add(keras.layers.Dropout(0.2))
+model.add(keras.layers.BatchNormalization())
+
+model.add(keras.layers.Conv2D(64, 3, activation='relu', padding='same'))
+model.add(keras.layers.MaxPooling2D(2))
+model.add(keras.layers.Dropout(0.2))
+model.add(keras.layers.BatchNormalization())
+
+model.add(keras.layers.Conv2D(64, 3, activation='relu', padding='same'))
+model.add(keras.layers.MaxPooling2D(2))
+model.add(keras.layers.Dropout(0.2))
+model.add(keras.layers.BatchNormalization())
+
+model.add(keras.layers.Conv2D(128, 3, activation='relu', padding='same'))
+model.add(keras.layers.Dropout(0.2))
+model.add(keras.layers.BatchNormalization())
+
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dropout(0.2))
+
+model.add(keras.layers.Dense(32, activation='relu'))
+model.add(keras.layers.Dropout(0.3))
+model.add(keras.layers.BatchNormalization())
+
+model.add(keras.layers.Dense(class_num, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', 'val_accuracy'])
+print(model.summary())
+
+numpy.random.seed(seed)
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=25, batch_size=64)
+
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
