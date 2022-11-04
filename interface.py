a9317dd43d02e64ae6d4427e574b2751cd2dfa1e
Jordan Jung <jjung2@oxy.edu>
2022-11-04 16:55:45 -0700

    interface fixes

--- interface.py
+++ interface.py
@@ -5,8 +5,10 @@ import re
 import os
 import Utils.myFuncs as myFuncs
 
+
 class MyWindow:
     def __init__(self, win):
+        champ_list = myFuncs.open_file()
         myDict = {}
         dx = 25
         dy = 25
@@ -18,17 +20,17 @@ class MyWindow:
         for j in range(0, 5):  # 1 costs
             for i in range(len(champ_list[j])):
                 champ = champ_list[j][i]
-                myDict["button_" + str(i)+str(j)] = ttk.Button(window, text=champ_list[j][i], command=lambda i=i, j=j: [
+                myDict["button_" + str(i)+str(j)] = ttk.Button(win, text=champ_list[j][i], command=lambda i=i, j=j: [
                     myFuncs.add(cur_list, champ_list[j][i]), start(myDict["button_" + str(i)+str(j)])], style=SUNKABLE_BUTTON)
                 myDict["button_" + str(i)+str(j)].place(x=dx * (j*5), y=dy * i)
 
         # Print list button
-        print_list = Button(window, text='print',
+        print_list = Button(win, text='print',
                             command=lambda: print(cur_list))
         print_list.place(x=400, y=400)
 
         # Clear list button
-        clear_list = Button(window, text="clear", command=lambda: [
+        clear_list = Button(win, text="clear", command=lambda: [
                             cur_list.clear(), clearButtons()])
         clear_list.place(x=500, y=400)
 
@@ -51,13 +53,14 @@ class MyWindow:
         def getList():
             return cur_list
 
+
 def main():
-    champ_list = myFuncs.open_file()
     window = Tk()
     mywin = MyWindow(window)
     window.title('Champs List')
     window.geometry("800x500+10+10")
     window.mainloop()
+
 
 if __name__ == "__main__":
     main()
